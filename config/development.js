7e5501a656fc21e1716ee9f6ddcf64a9c85220f0
CWD <cwd@cloudspokes.com>
2016-06-21 23:59:53 -0400

    oh mongo lab...

--- config/development.js
+++ config/development.js
@@ -2,7 +2,7 @@
 
 var config = {};
 
-config.db = (process.env.MONGOLAB_URI || ('mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') +
+config.db = (process.env.MONGODB_URI || ('mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') +
     '/slib'));
 config.SLACK_CLIENTID = process.env.SLACK_CLIENTID || '';
 config.SLACK_CLIENTSECRET = process.env.SLACK_CLIENTSECRET || '';
